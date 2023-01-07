300bf3d0fda46c074ba31ad1e089491b0a7e3b31
Manue777 <lejeune.manu@gmail.com>
2023-01-06 20:24:57 -0500

    actualizacion de flask con mysql

--- Dia-3/flask_con_db.py
+++ Dia-3/flask_con_db.py
@@ -7,78 +7,133 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-app = Flask (__name__)
-#cuando tenemos un diccionario poder obtener el valor de una de sus llaves con el metodo obtener , no para asignar . Y si que al obtener no hay valor entonces colocara none
-#esto no se peude hacer
-#environ.get (MySQL_HOST)="hola"
-#environ(MySQL_HOST)="hola"
-
+app = Flask(__name__)
+# cuando tenemos un diccionario poder OBTENER el valor de una de sus llaves con el metodo .get('LLAVE'), SOLO es para obtener, no para asignar. Y si que al obtener no hay valor entonces colocara None (vacio)
+# ESTO NO SE PUEDE HACER:
+# environ.get('MYSQL_HOST') = 'hola'
+# environ['MYSQL_HOST'] = 'hola'
 app.config['MYSQL_HOST'] = environ.get('MYSQL_HOST')
 app.config['MYSQL_USER'] = environ.get('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = environ.get('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = environ.get('MYSQL_DB')
 app.config['MYSQL_PORT'] = int(environ.get('MYSQL_PORT'))
+
 # Cuando a una variable se le asigna una clase se llama INSTANCIA
 # inicializamos la conexion seteando todos los parametros de nuestra bd, PERO AUN NO NOS HEMOS CONECTADO
 mysql = MySQL(app)
 
-@app.route("/productos", methods= ["GET", "POST"])
+@app.route('/productos', methods=['GET', 'POST'])
 def gestion_productos():
-    if request.method == "GET":
-        #crea una conexion a la base de datos mediante un cursor
-        cursor=mysql.connection.cursor()
+    if request.method == 'GET':
+        # crea una conexion a la base de datos mediante un cursor
+        cursor = mysql.connection.cursor()
         cursor.execute("SELECT * FROM productos")
-        productos =cursor.fetchall()  #limt infinito
+        productos = cursor.fetchall() # LIMIT infinito
         # cursor.fetchone() # LIMIT 1
         # print(productos)
         # cerra nuestra conexion
         cursor.close()
         resultado = []
         for producto in productos:
-            producto_dic ={
-                "id":producto[0],
-                "nombre":producto[1],
-                "imagen":producto[2],
-                "fehca_vencimiento":producto[3],
-                "precio":producto[4],
-                "disponible":producto[5],
-                "categoria_id":producto[6],
+            producto_dic = {
+                'id': producto[0],
+                'nombre': producto[1],
+                'imagen': producto[2],
+                'fecha_vencimiento': producto[3].strftime('%Y-%m-%d'), # %H:%M:%S
+                'precio': producto[4],
+                'disponible': producto[5],
+                'categoria_id': producto[6]
             }
             resultado.append(producto_dic)
             print(producto_dic)
-
         return {
-             "message":"los productos son",
-             "content": resultado 
+            'message': 'Los productos son',
+            'content': resultado
         }
-
-    elif request.method == "POST":
+    elif request.method == 'POST':
         cursor = mysql.connection.cursor()
-        informacion = request.get_json()  # un diccionario
+        informacion = request.get_json() # un diccionario
         # %s > convierte el contenido a un string
         # %f > convierte el contenido a un numero flotante
         # %d > convierte el contenido a un numero entero
-        cursor.execute("INSERT INTO productos (id, nombre, imagen, fecha_vencimiento, precio, disponible, categoria_id)  VALUES(DEFAULT,'%s','%s','%s', %f,%s ,%d )" % (
-            informacion.get("nombre"),
-            informacion.get("imagen"),
-            informacion.get("fecha_vencimiento"),
-            informacion.get("precio"),
-            informacion.get("disponible"),
-            informacion.get("categoria_id")
+        cursor.execute("INSERT INTO productos (id, nombre, imagen, fecha_vencimiento, precio, disponible, categoria_id) VALUES (DEFAULT, '%s', '%s', '%s', %f, %s, %d)" % (
+            informacion.get('nombre'),
+            informacion.get('imagen'), 
+            informacion.get('fecha_vencimiento'),
+            informacion.get('precio'),
+            informacion.get('disponible'),
+            informacion.get('categoria_id')
             ))
         # indicamos que el guardado sea permanente en la base de datos
         mysql.connection.commit()
         cursor.close()
+        
+        return {
+            'message': 'Producto creado exitosamente'
+        }
 
 
-        return{
-            "message": "productos creados exitosamente"
-        }  
+def validar_producto(id):
+    # creamos la conexion
+    conexion = mysql.connection.cursor()
+    # ejecutamos el select con la clausula del id
+    conexion.execute("SELECT * FROM productos WHERE id=%d" % (id))
+    # conexion.execute("SELECT * FROM productos WHERE id="+str(id))
+    # conexion.execute("SELECT * FROM productos WHERE id={}".format(id))
+    # conexion.execute(f"SELECT * FROM productos WHERE id={id}")
+    resultado = conexion.fetchone()
+    conexion.close()
+    print(resultado)
+    return resultado
 
 @app.route("/producto/<int:id>", methods = ['GET', 'PUT', 'DELETE'])
 def gestion_un_producto(id):
-    pass
+    if request.method == 'GET':
+        resultado = validar_producto(id)
+        if resultado is None:
+            return {
+                'message': 'Producto no encontrado'
+            }
+        else:
+            producto = {
+                    'id': resultado[0],
+                    'nombre':resultado[1],
+                    'imagen':resultado[2],
+                    'fecha_vencimiento':resultado[3].strftime('%Y-%m-%d'),
+                    'precio': resultado[4],
+                    'disponible': resultado[5],
+                    'categoria_id': resultado[6]
+                }
+            
+            return {
+                'content': producto
+            }
+    elif request.method == 'PUT':
+        resultado = validar_producto(id)
+        if resultado is None:
+            return {
+                'message': 'Producto no existe'
+            }
+        else:
+            data=request.get_json()
+            print(data)
+            conexion = mysql.connection.cursor()
+            conexion.execute("UPDATE productos SET nombre= %s, precio= %s, fecha_vencimiento= %s, categoria_id=%s, disponible=%s, imagen=%s WHERE id=%s", [
+                data.get('nombre'),
+                data.get('precio'),
+                data.get('fecha_vencimiento'),
+                data.get('categoria_id'),
+                data.get('disponible'),
+                data.get('imagen'),
+                resultado[0] # id del producto
+            ])
+            mysql.connection.commit()
+            conexion.close()
+
+            return {
+                'message': 'Producto actualizado exitosamente'
+            }
+
 
 # load_dotenv > cargamos todas las variables definidas en el archivo .env como si fueran variables de entorno
-app.run(debug=True)
-
+app.run(debug=True, load_dotenv=True)
